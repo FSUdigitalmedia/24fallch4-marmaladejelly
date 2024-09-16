ad4935a54521ef39001c757a6707f7cec4ce2f05
Amanda Anderson <ada23a@fsu.edu>
2024-09-16 16:28:27 -0400

    moving square

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let speed = 3;
 let y;
 let x;
+let rectX = ２；
+let rectY = 50；
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -22,40 +24,30 @@ function draw() {
   // output range
   // easing type
   // start, end, both
-  
   let angle = map2(y, 0, height, 0, TWO_PI, QUADRATIC, BOTH);
   let dia = map2(y, 0, height, 50, 50, SINUSOIDAL, BOTH);
 
-
-  /*// Enable orbiting with the mouse.
-  // Style the box.
-  // Draw the box.
-  orbitControl();
-  normalMaterial();
-  rectMode(CENTER);
-  box(30, 50);
-  */
-
   push();
-  translate(width/2, height/2);
+  translate(wi1 ```````11dth/2, height/2);
   rotate(angle);
   rectMode(CENTER);
   fill(255);
-  rotate(45)
-  square(0, 0, dia);
+  rotate(45);
+  square(rectX, rectY, 400, 20);
   pop();
-
-/*
-  if (circle1X > circle2X - circle2Width && circle1X < circle2X + circle2Width && circle1Y > circle2Y - circle2Height && circle1Y < circle2Y + circle2Height)
-
-  // Rotate square system 1/8 turn.
-  translate(width/2, height/2);
-  rotate(45)
-  square(0, 0, dia);
-*/
-
+    
   y += speed;
-  if (y < 25 || y > height-25) {
+  if (y < 25 || y > height - 25) {
     speed *= -1;
   }
+
+  rectTop      = rectY;
+  rectLeft     = rectX;
+  rectRight    = rectX + 400;
+  circleBottom = circle + 20;
+
+  if (circleBottom > rectTop && circle >= rectLeft && circle  <= rectRight ) {
+      velocityY= -5
+  }
+
 }
