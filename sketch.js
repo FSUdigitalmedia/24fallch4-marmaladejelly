f17a1bbfcfaeec8f18c9adbd451af659c47f84f5
Amanda Anderson <ada23a@fsu.edu>
2024-09-16 09:25:54 -0400

    added second curcle

--- sketch.js
+++ sketch.js
@@ -15,6 +15,16 @@ function draw() {
   noStroke();
   circle(width/2, y, 40);
 
+  // eased
+  // input value
+  // inout range
+  // output range
+  // easing type
+  // start, end, both
+  let easedY = map2(y, 0, height, 0, height, SINUSOIDAL, BOTH)
+  fill(0,150,255);
+  circle(width-width/4, easedY, 40);
+
   y += speed;
   if (y < 25 || y > height-25) {
     speed *= -1;
