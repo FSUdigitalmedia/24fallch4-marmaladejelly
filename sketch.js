4a8f2c00466f4adee6784262a315169753807917
Amanda Anderson <ada23a@fsu.edu>
2024-09-16 09:11:59 -0400

    circle moving

--- sketch.js
+++ sketch.js
@@ -1,8 +1,22 @@
+let speed = 3;
+let y;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+
+  y = height/2;
+
 }
 
 function draw() {
-  background(125);
-  circle(width/2, height/2, 40);
+  background(50);
+
+  fill(255,150,0)
+  noStroke();
+  circle(width/2, y, 40);
+
+  y += speed;
+  if (y < 25 || y > height-25) {
+    speed *= -1;
+  }
 }
